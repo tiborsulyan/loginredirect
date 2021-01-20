8e4b1f5c223a8921c62552b7ebd0a7fa282d0058
Tibor Sulyán <sulyan.tibor@gmail.com>
2021-01-20 23:09:03 +0100

    Update ExecuteRoute.php
    Fix comments

--- src/ExecuteRoute.php
+++ src/ExecuteRoute.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
- * This file is part of Flarum.
- *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
-
 namespace Tiborsulyan\LoginRedirect;
 
 use Flarum\Http\Exception\RouteNotFoundException;
