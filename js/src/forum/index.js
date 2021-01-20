a9b644fdf91b8567c6fe30ce191c291e3c1dbbfa
stibi <sulyan.tibor@gmail.com>
2021-01-20 23:37:19 +0100

    show login modal by default

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,16 +5,25 @@ import Page from "flarum/components/Page";
 
 /* global m */
 
+const isEmpty = function (o) {
+    return Object.keys(o).length === 0 && o.constructor === Object;
+};
+
 app.initializers.add('tiborsulyan/loginredirect', () => {
 
     extend(Page.prototype, 'oninit', function () {
         const urlParams = new URLSearchParams(window.location.search);
         const notfound = urlParams.get('notfound');
         const dest = urlParams.get('dest');
-
-        if (!app.session.user && notfound && dest) {
-            setTimeout(() => app.modal.show(LogInModal));
-        } else if (app.session.user && notfound && dest) {
+        console.log("current.type", app.current.type);
+        console.log("current.data", app.current.data);
+        console.log("prev.type", app.previous.type);
+        console.log("prev.data", app.previous.data);
+        if (!app.session.user) {
+            if (notfound && dest || isEmpty(app.previous.data) && location.path === "/") {
+                setTimeout(() => app.modal.show(LogInModal));
+            }
+        } else if (notfound && dest) {
             m.route.set(dest);
         }
     });
